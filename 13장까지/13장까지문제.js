a399764d4594f48e723b80aee75637dbdc612ea6
jongho <1jongho1227@gmail.com>
2023-10-11 20:04:57 +0900

    자판기 하는중

--- 13장까지/13장까지문제.js
+++ 13장까지/13장까지문제.js
@@ -4,30 +4,43 @@ arguments(1, 2); // 3
 arguments(1, 2, 3); // 6
 arguments(1, 2, 3, 4); // 10
 arguments(1, 2, 3, 4, 5); // 15
-function arguments() {}
+function arguments(...rest) {
+  let result = 0;
+  for (let i = 0; i < rest.length; i++) {
+    result += rest[i];
+  }
+  return result;
+}
 //2. var, let, const 의 차이점을 호이스팅과 관련지어서 설명
 
 //3. Falsy 한 값 아는대로 적고 false 와 falsy의 차이점 적기
+
 {
   //4.
-  const a = '가나' && (!'다라' || !'마바'); // a = ?
+  const a = '가나' || !'다라' || !'마바'; // a = ?
   //5. 위 a값에 '가나' 가 할당되게 논리 연산자를 바꾸기
 }
 
 {
   //6. 아래 a를 true로 바꾸기
-  const a = 'aa';
+  const a = !!'aa';
 }
 
 {
   //7. 아래의 객체에 age 프로퍼티 추가하고 자기 나이 넣기
   const a = { name: '이름' };
+  //a.age = 28;
+  //a['age'] = 28;
   //8. move라는 메소드 추가하기
+  a.move = function () {
+    console.log('a');
+  };
+  console.log(a.move());
 }
 
 {
   //9.다음 코드에서 잘 못된 부분 수정하기
-  const a = { name: 'product', cnt: 10 };
+  const a = { name: 'product', cnt: 10 }; //객체
   function product(product) {
     if (product.cnt < 10) product.cnt += 10;
   }
@@ -39,9 +52,7 @@ function arguments() {}
   function fn(f, l) {
     let num = 10;
     return function () {
-      for (let i = 0; i < l.length; i++) {
-        num += f(l[i]);
-      }
+      for (let i = 0; i < l.length; i++) num += f(l[i]);
       return num;
     };
   }
--- 자판기/자판기.js
+++ 자판기/자판기.js
@@ -6,3 +6,75 @@ const item = [
   { name: '제로콜라', price: 1200, count: 5 },
   { name: '환타', price: 900, count: 5 },
 ];
+
+function chunkArray(array, chunkSize) {
+  const result = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    result.push(array.slice(i, i + chunkSize));
+  }
+  return result;
+}
+function makeTable() {
+  let newItem = chunkArray(item, 3);
+  const $body = document.body;
+  $body.innerHTML = `<table id="자판기">
+                      <thead>
+                        <tr>
+                          <td colspan="3">자판기</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        ${newItem
+                          .map((item) => {
+                            let tr = '<tr>';
+                            item.map((i) => {
+                              tr += `<td class="drink" data-name="${i.name}" data-price="${i.price}">${i.name}(${i.price})</td>`;
+                            });
+                            tr += '</tr>';
+                            return tr;
+                          })
+                          .join('')}
+                          <tr>
+                        <td>
+                          <button type="button" id="insertMoney">돈 넣기</button>
+                        </td>
+                        <td>잔액 : <span id="money">0</span></td>
+                        <td></td>
+                      </tr>
+                      <tr>
+                        <td colspan="3" id="myItem">뽑은 음료수</td>
+                      </tr>
+                      </tbody>
+                      </table>`;
+}
+makeTable();
+const $moneyButton = document.querySelector('#insertMoney');
+
+$moneyButton.addEventListener('click', () => {
+  //돈을 입력받는다.
+  const money = prompt('돈을 넣어주세요');
+  if (money === null) return;
+  if (isNaN(Number(money))) return;
+  const $money = document.getElementById('money');
+  const totalPrice = Number(money) + Number($money.textContent);
+  $money.textContent = totalPrice;
+
+  //살수있는음료수에 불을 킨다.
+
+  //1. 아이템이 들어있는 td를 가지고 온다 // ok
+  //2. 아이템의 가격이랑 잔액이랑 비교한다.
+  //3. 가격이 잔액보다 작거나 같으면 불을 킨다.
+
+  const $tds = document.querySelectorAll('.drink');
+  $tds.forEach(($td) => {
+    const price = $td.getAttribute('data-price');
+    if (totalPrice - price >= 0) {
+      $td.classList.add('buyable');
+    } else {
+      $td.classList.remove('buyable');
+    }
+  });
+});
+//여기까지한거 혼자 해봐요.
+//음료수 누르면 잔액에서 음료수 가격만큼 빼기
+//다시 잔액으로 살수있는 애들만 불들어오게 하기
